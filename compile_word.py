49366b8687af19bf9c371baff01e9a131d11fbee
kuma300 <bb.kaipro@gmail.com>
2018-05-20 11:24:13 +0800

    add 3 py files

---
+++ compile_word.py
@@ -0,0 +1,7 @@
+def compile_word(word):
+    if word.isupper():
+        terms = [('%s*%s' % (10**i, d))
+                 for (i, d) in enumerate(word[::-1])]
+        return '(' + '+'.join(terms) + ')'
+    else:
+        return word
